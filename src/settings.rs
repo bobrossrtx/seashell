21a03d1101886c50898c29b43a1619b94b4da7b1
bobrossrtx <bobrossrtx@gmail.com>
2025-03-28 01:43:11 +0000

    Add version and author fields to Settings struct with default values

--- src/settings.rs
+++ src/settings.rs
@@ -7,6 +7,8 @@ lazy_static::lazy_static! {
 #[derive(Debug, Clone)]
 pub struct Settings {
     pub app_name: String,
+    pub version: String,
+    pub author: String,
     pub max_connections: u32,
     pub enable_logging: bool,
     pub log_file_path: String,
@@ -14,10 +16,13 @@ pub struct Settings {
     pub history_file_path: String,
 }
 
+// This is default settings (Change it if you want (e.g. app_name, version, etc))
 impl Default for Settings {
     fn default() -> Self {
         Settings {
             app_name: "Seashell".to_string(),
+            version: "0.1.0".to_string(),
+            author: "Bobrosssrtx".to_string(),
             max_connections: 100,
             enable_logging: true,
             log_file_path: "~/.seashell/seashell.log".to_string(),
