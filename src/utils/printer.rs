339214bfa26567d9f00aae02a44a8264d4a825f3
bobrossrtx <bobrossrtx@gmail.com>
2025-03-28 01:43:17 +0000

    Refactor Printer module to standardize message color to white for all log levels

--- src/utils/printer.rs
+++ src/utils/printer.rs
@@ -2,32 +2,33 @@ use colored::*;
 
 pub struct Printer;
 
+#[allow(dead_code)]
 impl Printer {
     pub fn error(message: &str) {
         println!(
             "{}",
-            format!("[{}]: {}", "ERROR".bold().bright_red(), message).red()
+            format!("[{}]: {}", "ERROR".bold().bright_red(), message).white()
         );
     }
 
     pub fn debug(message: &str) {
         println!(
             "{}",
-            format!("[{}]: {}", "DEBUG".bold().bright_cyan(), message).blue()
+            format!("[{}]: {}", "DEBUG".bold().bright_cyan(), message).white()
         );
     }
 
     pub fn info(message: &str) {
         println!(
             "{}",
-            format!("[{}]: {}", "INFO".bold().bright_blue(), message).green()
+            format!("[{}]: {}", "INFO".bold().bright_blue(), message).white()
         );
     }
 
     pub fn warning(message: &str) {
         println!(
             "{}",
-            format!("[{}]: {}", "WARNING".bold().bright_yellow(), message).yellow()
+            format!("[{}]: {}", "WARNING".bold().bright_yellow(), message).white()
         );
     }
 
